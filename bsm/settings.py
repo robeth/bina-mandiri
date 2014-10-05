158a1d7114a00f487b3efbcce41a2a92cd363405
robeth <robethrahmatullah@gmail.com>
2014-10-05 14:47:15 +0700

    Add logging module

--- bsm/settings.py
+++ bsm/settings.py
@@ -98,6 +98,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -126,6 +127,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'transaction',
+    'debug_toolbar',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -158,3 +160,5 @@ LOGGING = {
 }
 
 LOGIN_URL= '/trans/login/'
+
+INTERNAL_IPS = ('127.0.0.1',)
