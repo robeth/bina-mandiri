6af19741f0115faabddf47cfd57815da2547f0f9
beth <robethrahmatullah@gmail.com>
2015-08-20 09:28:07 +0700

    update initial balance script

--- script/test.py
+++ script/test.py
@@ -1,14 +1,19 @@
 import csv
-# from transaction.models import Pembelian, Stok, Kategori
-
-
+import datetime
+from transaction.models import Pembelian, Stok, Kategori, Nasabah
 
 saldo_dict = {}
-tanggal = '2015-06-14'
+tanggal = datetime.datetime.now().strftime("%Y-%m-%d")
 
+# Stok.objects.all().delete()
+# Pembelian.objects.all().delete()
 
-with open('test.csv', 'r') as csvfile:
-  saldo_reader = csv.reader(csvfile, delimiter=',')
+# Create dictionary
+#  {
+# 	NASABAH_ID : SALDO
+#  }
+with open('input aug kolektif.csv', 'r') as csvfile:
+  saldo_reader = csv.reader(csvfile, delimiter=';')
   for row in saldo_reader:
     saldo_dict[row[0]] = row[1]
 
@@ -19,9 +24,10 @@ for nasabah_id, saldo in saldo_dict.items():
   p.save()
   k = Kategori.objects.get(id=1000)
   s = Stok(kategori = k,
-    tanggal = '2015-06-14',
+    tanggal = '2015-08-18',
     jumlah = 1,
     harga = saldo)
+  #print "N%s - Rp %s" % (nasabah_id, saldo)
   s.save()
   p.stocks.add(s)
   p.save()
